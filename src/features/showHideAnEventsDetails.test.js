97c9eb3c621fa3da948f93da561232bb4c6896f5
jwho85 <98196552+jwho85@users.noreply.github.com>
2022-07-13 11:04:16 -0700

    Updated feature tests

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,56 +1,66 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import App from '../App';
-import { mockData } from '../mock-data';
+import { loadFeature, defineFeature } from "jest-cucumber";
+import React from "react";
+import { mount } from "enzyme";
+import App from "../App";
 
-const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
+const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
 let AppWrapper;
 
-defineFeature(feature, test => {
-    test('An event element is collapsed by default', ({ given, when, then }) => {
-        given('the app is open', () => {
+defineFeature(feature, (test) => {
+    test("An event element is collapsed by default", ({ given, when, then }) => {
+        given("the app is open", () => {
             AppWrapper = mount(<App />);
         });
 
-        when('an event is displayed', () => {
+        when("an event is displayed", () => {
             AppWrapper.update();
-            expect(AppWrapper.find('.event-box')).toHaveLength(2);
+            expect(AppWrapper.find(".event-box")).toHaveLength(2);
         });
 
-        then('the event details should be collapsed by default', () => {
-            expect(AppWrapper.find('.event-box .event-details')).toHaveLength(0);
+        then("the event details should be collapsed by default", () => {
+            expect(AppWrapper.find(".event-box .event-details")).toHaveLength(0);
         });
     });
 
-    test('User can expand an event to see its details', ({ given, when, then }) => {
-        given('the app is open', () => {
+    test("User can expand an event to see its details", ({
+        given,
+        when,
+        then,
+    }) => {
+        given("the app is open", () => {
             AppWrapper = mount(<App />);
         });
 
-        when('the user clicks on the event button', () => {
+        when("the user clicks on the event button", () => {
             AppWrapper.update();
-            AppWrapper.find('.event-box .show-details-button').at(0).simulate('click');
+            AppWrapper.find(".event-box .show-details-button")
+                .at(0)
+                .simulate("click");
         });
 
-        then('the event details will be displayed', () => {
-            expect(AppWrapper.find('.event-box .event-details')).toHaveLength(1);
+        then("the event details will be displayed", () => {
+            expect(AppWrapper.find(".event-box .event-details")).toHaveLength(1);
         });
     });
 
-    test('User can collapse an event to hide its details', ({ given, when, then }) => {
-        given('the user has already opened an event', () => {
-            AppWrapper = mount(<App />);
-            AppWrapper.find('.event-box .show-details-button').at(0).simulate('click');
-        });
-
-        when('the user clicks on the event', () => {
+    test("User can collapse an event to hide its details", ({
+        given,
+        when,
+        then,
+    }) => {
+        given("the user has already opened an event", async () => {
+            AppWrapper = await mount(<App />);
             AppWrapper.update();
-            AppWrapper.find('.event-box .show-details-button').at(0).simulate('click');
+            AppWrapper.find(".show-details-button").at(0).simulate("click");
         });
 
-        then('the event collapses to hide its details', () => {
-            expect(AppWrapper.find('.event-box .event-details')).toHaveLength(0);
+        when("the user clicks on the event", () => {
+            AppWrapper.update();
+            AppWrapper.find(".show-details-button").at(0).simulate("click");
+        });
+
+        then("the event collapses to hide its details", () => {
+            expect(AppWrapper.find(".event-box .event-details")).toHaveLength(0);
         });
     });
 });
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,40 +1,49 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import App from '../App';
-import { mockData } from '../mock-data';
+import { loadFeature, defineFeature } from "jest-cucumber";
+import React from "react";
+import { mount, shallow } from "enzyme";
+import App from "../App";
+import { mockData } from "../mock-data";
 
-const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
+const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
 let AppWrapper;
 
-defineFeature(feature, test => {
-    test('When the user hasn\'t specified a number, 32 is the default number', ({ given, when, then }) => {
-        given('the app is open', () => {
+defineFeature(feature, (test) => {
+    test("When the user hasn't specified a number, 32 is the default number", ({
+        given,
+        when,
+        then,
+    }) => {
+        given("the app is open", () => {
             AppWrapper = mount(<App />);
         });
 
-        when('the user hasn\'t specified a number', () => {
+        when("the user hasn't specified a number", () => {
             AppWrapper.update();
         });
 
-        then('the number count should be 32', () => {
-            expect(AppWrapper.find('.event-box')).toHaveLength(2);
+        then("the number count should be 32", () => {
+            expect(AppWrapper.find(".event-box")).toHaveLength(2);
         });
     });
 
-    test('User can change the number of events they want to see', ({ given, when, then }) => {
-        given('the app is open', () => {
+    test("User can change the number of events they want to see", ({
+        given,
+        when,
+        then,
+    }) => {
+        given("the app is open", () => {
             AppWrapper = mount(<App />);
         });
 
-        when('the user changes the event count', () => {
-            AppWrapper.update();
-            AppWrapper.find('.number-input').simulate('change', { target: { value: 1 } });
+        when("the user changes the event count", () => {
+            AppWrapper.find(".number-input").simulate("change", {
+                target: { value: 1 },
+            });
         });
 
-        then('the number of events should change based on user input', () => {
-            expect(AppWrapper.find('.event-box')).toHaveLength(1);
+        then("the number of events should change based on user input", () => {
+            AppWrapper.update();
+            expect(AppWrapper.find(".event-box")).toHaveLength(1);
         });
     });
-
 });
