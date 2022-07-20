8571ab23bb2ff3846f1f2b958c90d415e783bff1
jwho85 <98196552+jwho85@users.noreply.github.com>
2022-07-20 13:27:30 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -20,17 +20,20 @@ class App extends Component {
     showWelcomeScreen: undefined,
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const isTokenValid = (checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
+          });
         }
       });
     }
